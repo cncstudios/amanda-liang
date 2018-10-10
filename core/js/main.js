988f60fc9c0ae1b3d6b9d96c18313d98a80def76
cncstudios <43526458+cncstudios@users.noreply.github.com>
2018-10-09 23:50:23 -0700

    Add files via upload
    Initial commit.

---
+++ core/js/main.js
@@ -0,0 +1,93 @@
+//Loading//
+document.onreadystatechange = function () {
+    var state = document.readyState
+    if (state == 'interactive') {
+        document.getElementById('body-container').style.opacity="0";
+  } else if (state == 'complete') {
+      setTimeout(function(){
+          document.getElementById('interactive');
+          document.getElementById('load').style.cssText="opacity:0;z-index:-2;";
+          document.getElementById('body-container').style.opacity="1";
+      },500);
+  }
+}
+
+//Smooth Scrolling//
+if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
+window.onmousewheel = document.onmousewheel = wheel;
+
+function wheel(event) {
+    var delta = 0;
+    if (event.wheelDelta) delta = event.wheelDelta / 60;
+    else if (event.detail) delta = -event.detail / 60;
+
+    handle(delta);
+    if (event.preventDefault) event.preventDefault();
+    event.returnValue = false;
+}
+var goUp = true;
+var end = null;
+var interval = null;
+
+function handle(delta) {
+	var animationInterval = 20;
+  var scrollSpeed = 20;
+
+	if (end == null) {
+  	end = $(window).scrollTop();
+  }
+  end -= 20 * delta;
+  goUp = delta > 0;
+
+  if (interval == null) {
+    interval = setInterval(function () {
+      var scrollTop = $(window).scrollTop();
+      var step = Math.round((end - scrollTop) / scrollSpeed);
+      if (scrollTop <= 0 || 
+          scrollTop >= $(window).prop("scrollHeight") - $(window).height() ||
+          goUp && step > -1 || 
+          !goUp && step < 1 ) {
+        clearInterval(interval);
+        interval = null;
+        end = null;
+      }
+      $(window).scrollTop(scrollTop + step );
+    }, animationInterval);
+  }
+}
+
+$(document).ready(function (){
+    var viewportHeight = $(window).outerHeight();
+    var viewportWidth = $(window).outerWidth();
+    var ratioFix = (viewportHeight/viewportWidth)
+    var heightCorrection = (ratioFix*100 + "vw");
+    var heightCorrection2 = (ratioFix*70 + "vw");
+    console.log(viewportHeight);
+    console.log(viewportWidth);
+    console.log(ratioFix);
+    console.log(heightCorrection);
+    $('.mobile-fix').children('div').css({ height: heightCorrection });
+    $('.slick-container').css({ height: heightCorrection });
+    //Animated Arrow//
+    (function($) {
+    $.fn.seqfx = function() {
+        var elements = this,
+            l = elements.length,
+            i = 0;
+
+        function execute() {
+            var current = $(elements[i]);
+            i = (i) % l;
+
+            current
+                .fadeIn(350)
+                .delay(1200)
+                .fadeOut(350, execute);
+        }
+        execute();
+        return this;
+    };
+}(jQuery));
+
+$(".down, h4").seqfx();
+});
